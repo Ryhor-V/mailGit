941d6941734d08266fe7ad621b81750102e71653
Ryhor <ryhormail@gmail.com>
2018-07-25 21:05:45 +0300

    write class AbstractPage

--- src/test/java/by/htp/test/pages/AbstractPage.java
+++ src/test/java/by/htp/test/pages/AbstractPage.java
@@ -1,5 +1,16 @@
 package by.htp.test.pages;
 
-public class AbstractPage {
+import org.openqa.selenium.WebDriver;
+
+public abstract class AbstractPage {
+	protected WebDriver driver;
+	
+	public abstract void openPage();
+	
+	public AbstractPage(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
 
 }
