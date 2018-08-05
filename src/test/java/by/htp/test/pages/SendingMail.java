c405c717f7423b64578d4c7f036fdd8ef0b21034
Ryhor <ryhormail@gmail.com>
2018-08-05 11:17:00 +0300

    Add class Sending mail

---
+++ src/test/java/by/htp/test/pages/SendingMail.java
@@ -0,0 +1,58 @@
+package by.htp.test.pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SendingMail extends AbstractPage {
+	private final String BASE_URL = "https://e.mail.ru/login";
+
+	public SendingMail(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(this.driver, this);
+
+	}
+
+	@FindBy(xpath = ".//iframe[contains(@id,'composeEditor_ifr')]")
+	private WebElement frameNewLetter;
+
+	@FindBy(xpath = ".//body[@class='mceContentBody compose2']")
+	private WebElement NewLetter;
+
+	@FindBy(xpath = "//child::textarea[attribute::data-original-name='To']")
+	private WebElement emailField;
+
+	@FindBy(xpath = ".//input[@class='b-input']")
+	private WebElement topic;
+
+	@FindBy(xpath = ".//body/iframe[@name='easyXDM_default3_provider']")
+	private WebElement frameWhom;
+
+	@FindBy(xpath = ".//div[@data-name='send']/span")
+	private WebElement button;
+
+	@Override
+	public void openPage() {
+		driver.navigate().to(BASE_URL);
+		System.out.println("Write Hello page opened");
+
+	}
+	
+	public void sendLetter(String em, String top, String message) {
+		WebElement e_mail = driver.switchTo().activeElement();
+		e_mail.clear();
+		e_mail.sendKeys(em);
+		e_mail.sendKeys(Keys.TAB);
+		e_mail.sendKeys(top);
+		
+		driver.switchTo().frame(frameNewLetter);
+		NewLetter.clear();
+		NewLetter.sendKeys(message);
+		driver.switchTo().defaultContent();
+		button.click();
+		
+	}
+
+}
